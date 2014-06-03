cf9e3368161b80afd837c9f547e305a71c70bec2
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-06-03 21:22:24 +0000

    layout: Add brown theme and cleanup

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,24 +12,6 @@ class RecipesController < ApplicationController
   def show
   end
 
-  # GET /recipes/1/edit
-  def edit
-  end
-
-  # PATCH/PUT /recipes/1
-  # PATCH/PUT /recipes/1.json
-  def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
