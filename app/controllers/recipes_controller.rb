ba0a70b22acfb880481063d0eecdc1b7cebc4b5c
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-10-02 10:33:38 -0400

    controller/recipes: Move n+1 solution to controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.includes(:packages).includes(:architectures).all
     @architectures = Architecture.all
   end
 
