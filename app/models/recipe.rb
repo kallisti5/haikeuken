c7e38b2023edb9fcb18f5e249713afd978270ea6
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-10-14 14:54:42 -0400

    models/recipe: Fix ordering of results

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,9 +6,9 @@ class Recipe < ActiveRecord::Base
   def self.search(search)
     if search
       # TODO: Better sort version numbers as they are strings
-      where("name LIKE ?", "%#{search}%").order(:version)
+      where("name LIKE ?", "%#{search}%").order([:category, :name, :version])
     else
-      all
+      all.order([:category, :name, :version])
     end
   end
 end
