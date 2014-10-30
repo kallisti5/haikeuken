96e8919bd4928a0c95dfe6bfbc733ba1b265a42e
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-10-30 16:51:50 -0400

    Views: Make views consistent

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -12,55 +12,6 @@ class BuildsController < ApplicationController
   def show
   end
 
-  # GET /builds/new
-  def new
-    @build = Build.new
-  end
-
-  # GET /builds/1/edit
-  def edit
-  end
-
-  # POST /builds
-  # POST /builds.json
-  def create
-    @build = Build.new(build_params)
-
-    respond_to do |format|
-      if @build.save
-        format.html { redirect_to @build, notice: 'Build was successfully created.' }
-        format.json { render :show, status: :created, location: @build }
-      else
-        format.html { render :new }
-        format.json { render json: @build.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /builds/1
-  # PATCH/PUT /builds/1.json
-  def update
-    respond_to do |format|
-      if @build.update(build_params)
-        format.html { redirect_to @build, notice: 'Build was successfully updated.' }
-        format.json { render :show, status: :ok, location: @build }
-      else
-        format.html { render :edit }
-        format.json { render json: @build.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /builds/1
-  # DELETE /builds/1.json
-  def destroy
-    @build.destroy
-    respond_to do |format|
-      format.html { redirect_to builds_url, notice: 'Build was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_build
