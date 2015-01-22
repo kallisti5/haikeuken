7ebfedf2cf79e3a99d5832d6abd77f9314064b40
Alexander von Gluck IV <kallisti5@unixzen.com>
2015-01-22 15:45:54 -0500

    Overall: General cleanup of paths, add startserver script

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
 worker_processes 2
-working_directory "/var/rails/hpbs/"
+working_directory "/opt/apps/haikeuken/"
  
 preload_app true
 timeout 120
-listen "/var/rails/hpbs/tmp/sockets/unicorn.sock", :backlog => 64
-pid "/var/rails/hpbs/tmp/pids/unicorn.pid"
+listen "/opt/apps/haikeuken/tmp/sockets/unicorn.sock", :backlog => 64
+pid "/opt/apps/haikeuken/tmp/pids/unicorn.pid"
 
 #     # Set the path of the log files inside the log folder of the testapp
-stderr_path "/var/rails/hpbs/log/unicorn.stderr.log"
-stdout_path "/var/rails/hpbs/log/unicorn.stdout.log"
+stderr_path "/opt/apps/haikeuken/log/unicorn.stderr.log"
+stdout_path "/opt/apps/haikeuken/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
 	defined?(ActiveRecord::Base) and
