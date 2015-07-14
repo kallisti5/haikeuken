1716e705eb140dcac8f874804f5373b206173ed1
Alexander von Gluck IV <kallisti5@unixzen.com>
2015-07-14 18:19:29 -0500

    rails: upgrade rails

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,8 @@ working_directory "/opt/apps/haikeuken/"
  
 preload_app true
 timeout 120
-listen "/opt/apps/haikeuken/tmp/sockets/unicorn.sock", :backlog => 64
+#listen "/opt/apps/haikeuken/tmp/sockets/unicorn.sock", :backlog => 64
+listen 3000
 pid "/opt/apps/haikeuken/tmp/pids/unicorn.pid"
 
 #     # Set the path of the log files inside the log folder of the testapp
