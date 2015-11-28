fa4931c556f91e9d846da261a62433129e39e655
Alexander von Gluck IV <kallisti5@unixzen.com>
2015-11-28 06:48:13 -0600

    view: Tooltip cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,22 @@ module ApplicationHelper
     if !package
       return image_tag "icons/working-bar.gif"
     else
+      age = package.recipe.revision - package.latestrev
       hpkgname = "#{package.recipe['name']}-#{package.recipe['version']}-#{package.latestrev}-#{package.architecture['name']}.hpkg"
       repo = "#{package.repo['url']}/#{package.architecture['name']}/current/packages"
       placement = (size == 16) ? "left" : "bottom"
-      tooltip = { :toggle => "tooltip", :placement => placement, :title => (package.latestrev != 0 ? package.latestrev : "no builds") }
+
+      if package.latestrev == 0
+        tooltip = { :toggle => "tooltip", :placement => placement, :title => "No builds found" }
+      elsif age == 0
+        tooltip = { :toggle => "tooltip", :placement => placement, :title => "Up to date" }
+      else
+        tooltip = { :toggle => "tooltip", :placement => placement, :title => "#{age} #{'build'.pluralize(age)} behind" }
+      end
+
       if package.latestrev == 0
         return image_tag "fc#{size}/box_none.png", :data => tooltip
-      elsif package.recipe.revision == package.latestrev
+      elsif age == 0
         return link_to image_tag("fc#{size}/box_green.png", :alt => package.latestrev, :data => tooltip), "#{repo}/#{hpkgname}"
       else
         return link_to image_tag("fc#{size}/box_old.png", :alt => package.latestrev, :data => tooltip), "#{repo}/#{hpkgname}"
